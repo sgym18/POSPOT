f932a7741d869b23deff2657d5e83a6bce388bd1
su-kino <snow.white.1818@gmail.com>
2022-04-05 02:58:19 +0000

    [Add]ゲストユーザーログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about' => 'homes#about'
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
