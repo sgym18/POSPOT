6caa04a597ec26d5f13c9e20582837503a6baf2b
su-kino <snow.white.1818@gmail.com>
2022-04-08 09:53:07 +0000

    [Add]フォロー機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,6 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   def get_image(width, height)
-    image.variant(resize_to_limit: [width, height]).processed
+    image.variant(resize_to_fill: [width, height]).processed
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_004124) do
+ActiveRecord::Schema.define(version: 2022_04_08_055302) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2022_04_06_004124) do
     t.string "address", null: false
     t.float "longitude"
     t.float "latitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
