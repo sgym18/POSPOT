4bf2130275f5e2e6fa3e8df1832ab617e144c170
su-kino <snow.white.1818@gmail.com>
2022-04-11 09:17:17 +0000

    [Fix]退会機能sessions_controller修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
   before_action :user_state, only: [:create]
 
   def guest_sign_in
@@ -12,8 +15,30 @@ class Users::SessionsController < Devise::SessionsController
   def user_state
     @user = User.find_by(email: params[:user][:email])
     return if !@user
-    if @user.valid_password?(params[:user][:pasaword]) && @user.is_deleted
-      redirect_to new_user_registration_path
+    if @user.valid_password?(params[:user][:password]) && @user.is_deleted
+      redirect_to new_user_registration_path, notice: '退会済みユーザーのため新規登録が必要です。'
     end
   end
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
