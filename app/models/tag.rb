00233d42b8f8a531ee6b5be397c45fc422adf10c
su-kino <snow.white.1818@gmail.com>
2022-04-12 03:41:22 +0000

    [Add]検索フォームレイアウト調整、タグ検索コメントアウトで実装（暫定）

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ApplicationRecord
   has_many :post_tags, foreign_key: "tag_id", dependent: :destroy
   has_many :posts, through: :post_tags
+
+  # def self.search_for(keyword)
+  #   tags = Tag.where("name LIKE?", "%#{keyword}%")
+  #   return tags.inject(init = []) {|result, tag| result + tag.posts}
+  # end
 end
