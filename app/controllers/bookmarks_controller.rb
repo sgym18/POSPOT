cd9eca090728d718ee830687edf7c2423397d28c
su-kino <snow.white.1818@gmail.com>
2022-04-16 16:32:57 +0000

    [Add]ブックマーク、コメント、フォロー、管理者のユーザー編集非同期化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,14 @@
 class BookmarksController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    bookmark = current_user.bookmarks.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    bookmark = current_user.bookmarks.new(post_id: @post.id)
     bookmark.save
-    redirect_to request.referer
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    bookmark = current_user.bookmarks.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    bookmark = current_user.bookmarks.find_by(post_id: @post.id)
     bookmark.destroy
-    redirect_to request.referer
   end
-  
 end
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,17 @@
 class PostCommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    post_comment = current_user.post_comments.new(post_comment_params)
-    post_comment.post_id = post.id
-    post_comment.save
-    redirect_to request.referer, notice: "コメントを送信しました。"
+    @post = Post.find(params[:post_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.post_id = @post.id
+    @post_comment.save
+    flash.now[:notice] = "コメントを送信しました。"
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to request.referer, notice: "コメントを削除しました。"
+    PostComment.find_by(post_id: params[:post_id], id: params[:id]).destroy
+    @post = Post.find(params[:post_id])
+    flash.now[:notice] = "コメントを削除しました。"
   end
 
   private
