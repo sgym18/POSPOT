5e2c2c3221b739670b886839f441e1ba9b19ea58
su-kino <snow.white.1818@gmail.com>
2022-04-22 08:43:57 +0000

    [Fix]ページネーション削除

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,23 +2,8 @@ class SearchesController < ApplicationController
 
   def search
     @keyword = params[:keyword]
-    @user_result = Kaminari.paginate_array(User.search_for(@keyword)).page(params[:user_page]).per(4)
-    @post_result = Kaminari.paginate_array(Post.search_for(@keyword)).page(params[:post_page]).per(3)
-    @tag_result = Kaminari.paginate_array(Tag.search_for(@keyword)).page(params[:tag_page]).per(3)
-  end
-
-  def search_post
-    @keyword = params[:keyword]
-    @post_result = Kaminari.paginate_array(Post.search_for(@keyword)).page(params[:post_page]).per(3)
-  end
-
-  def search_tag
-    @keyword = params[:keyword]
-    @tag_result = Kaminari.paginate_array(Tag.search_for(@keyword)).page(params[:tag_page]).per(3)
-  end
-
-  def search_user
-    @keyword = params[:keyword]
-    @user_result = Kaminari.paginate_array(User.search_for(@keyword)).page(params[:user_page]).per(4)
+    @user_result = User.search_for(@keyword)
+    @post_result = Post.search_for(@keyword)
+    @tag_result = Tag.search_for(@keyword)
   end
 end
